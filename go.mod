9000bfea81cb42f194504bd5f18e7ea4c6f58ee9
Aly Ragab <alyragab70@gmail.com>
2020-11-09 05:27:53 +0200

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module vault-curl2Go
 
 go 1.15
+
+require (
+	github.com/alessiosavi/Requests v0.3.7
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/hashicorp/go-retryablehttp v0.6.8 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/vault/api v1.0.4
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/onrik/logrus v0.8.0 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.7.0
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+	golang.org/x/sys v0.0.0-20201107080550-4d91cf3a1aaf // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+)
---
+++ policy.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/hashicorp/vault/api"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	token          = os.Getenv("VAULT_TOKEN")
+	vaultAddr      = os.Getenv("VAULT_ADDR")
+	policyEndPoint = vaultAddr + "/v1/sys/policies/acl/"
+)
+
+// Payload is the policy content
+type Payload struct {
+	Name         string   `json:"name"`
+	Path         string   `json:"path"`
+	Capabilities []string `json:"capabilities"`
+}
+
+func putPolicy() {
+	policy := Payload{
+		Name:         "dev",
+		Path:         "*",
+		Capabilities: []string{"create", "read", "list"},
+	}
+	data, err := json.Marshal(policy)
+	if err != nil {
+		log.Error(err)
+	}
+	body := bytes.NewReader(data)
+	req, err := http.NewRequest("PUT", policyEndPoint, body)
+	if err != nil {
+		log.Error(err)
+	}
+	//req.Header.Set("Content-Type", "application/json")
+	//req.Header.Set("X-Vault-Token", token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Error(err)
+	}
+	defer resp.Body.Close()
+	fmt.Println("Policy is Created ...")
+}
+func main() {
+	config := &api.Config{
+		Address: vaultAddr,
+	}
+	client, err := api.NewClient(config)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	client.SetToken(token)
+	log.Info("Client is Authenticated Successfully ..")
+
+	putPolicy()
+}
